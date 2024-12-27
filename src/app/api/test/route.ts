2302c019d66265597007fc3767a1e95a1bce95f9
Victor <victor.owiti@zeraki.app>
2024-12-27 21:11:11 +0300

    Test

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,18 +1,17 @@
-
-
 import { NextResponse } from "next/server";
-
 
 // Handle GET request to fetch all events
 export async function GET() {
   try {
     // Connect to the database
-   
 
     // Return the events
     return NextResponse.json("Api up", { status: 200 });
   } catch (error) {
     console.error("Error fetching events:", error);
-    return NextResponse.json({ error: "Failed to fetch events." }, { status: 500 });
+    return NextResponse.json(
+      { error: "Failed to fetch events." },
+      { status: 500 }
+    );
   }
 }
