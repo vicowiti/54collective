1c210eca21d1a16b120d243d45f3e94d02413c83
Victor <victor.owiti@zeraki.app>
2024-12-28 09:21:30 +0300

    Eso table

--- src/components/EsoTable.tsx
+++ src/components/EsoTable.tsx
@@ -47,44 +47,45 @@ const EsoTable = () => {
     return <p>{error}</p>;
   }
 
-  return (
-    <section>
-      <table className="w-full">
-        <thead className="bg-[#80C22F] text-white font-semibold sm:font-bold text-sm sm:text-lg">
-          <tr>
-            <th className="sm:p-3 py-2 text-left">Name</th>
-            <th className="p-3  text-left hidden sm:table-cell">ESO Type</th>
-            <th className="p-3  text-left hidden sm:table-cell">
-              Support Type
-            </th>
+  
+    return (
+      <section>
+        <table className="w-full">
+          <thead className="bg-[#80C22F] text-white font-semibold sm:font-bold text-sm sm:text-lg">
+            <tr>
+              <th className="sm:p-3 py-2 text-left">Name</th>
+              <th className="p-3  text-left hidden sm:table-cell">ESO Type</th>
+              <th className="p-3  text-left hidden sm:table-cell">
+                Support Type
+              </th>
 
-            <th className="sm:p-3 py-2 text-left ">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.map((program, index) => (
-            <tr key={index} className="border-b border-[#D5D5D5]">
-              <td className="p-3">{program.companyName}</td>
-              <td className="p-3 hidden sm:table-cell">{program.esoType}</td>
-              <td className="p-3 hidden sm:table-cell">
-                {program.supportType}
-              </td>
-
-              <td className="p-3">
-                <a
-                  className="text-[#80C22F] whitespace-nowrap border border-[#80C22F] p-2 text-xs sm:p-2 rounded-md"
-                  href={program.website}
-                  target="_blank"
-                >
-                  View
-                </a>
-              </td>
+              <th className="sm:p-3 py-2 text-left ">Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </section>
-  );
+          </thead>
+          <tbody>
+            {data?.map((program, index) => (
+              <tr key={index} className="border-b border-[#D5D5D5]">
+                <td className="p-3">{program.companyName}</td>
+                <td className="p-3 hidden sm:table-cell">{program.esoType}</td>
+                <td className="p-3 hidden sm:table-cell">
+                  {program.supportType}
+                </td>
+
+                <td className="p-3">
+                  <a
+                    className="text-[#80C22F] whitespace-nowrap border border-[#80C22F] p-2 text-xs sm:p-2 rounded-md"
+                    href={program.website}
+                    target="_blank"
+                  >
+                    View
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    );
 };
 
 export default EsoTable;
