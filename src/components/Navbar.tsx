f88e644a0030639ac7caf66e7dbe77a051a05297
Victor <victor.owiti@zeraki.app>
2025-01-07 07:23:09 +0300

    Navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -89,7 +89,7 @@ const Navbar = () => {
                     <div className="flex items-center justify-end">
                       <div className="flex">
                         <button className="rounded-md px-3 py-2 text-sm font-medium text-white hover:text-[#e5cf00] duration-200 hover:scale-110">
-                          Solutions
+                          Resources
                         </button>
 
                         <div
@@ -97,7 +97,7 @@ const Navbar = () => {
                           onClick={() => setDropdownVisible(!dropdownVisible)}
                         >
                           <span className="rounded-md flex items-center gap-2 px-3 py-2 text-sm font-medium text-white hover:text-[#e5cf00] duration-200 hover:scale-110">
-                            Resources
+                            Solutions
                             <FaChevronDown />
                           </span>
 
