df206354d713e7eb057ed134807e0d8c7f46c553
Victor <victor.owiti@zeraki.app>
2025-01-13 14:11:11 +0300

    Resources update

--- src/data/resourceData.ts
+++ src/data/resourceData.ts
@@ -8,28 +8,29 @@ export const resourceData = [
     link: "https://www.inc.com/john-rampton/10-things-i-wish-i-knew-before-i-started-my-own-business.html",
   },
   {
-    title: "The Busy Trap",
+    title: "How Structure, Collaboration Can Stem High SME Mortality Rate",
     description:
-      "The Busy Trap explores how modern society glorifies busyness as a badge of honor, often at the expense of meaningful living.  The important takeaway  is that the busyness we fill our lives with must be purposeful and not consume all of who we are.",
+      "The article explores challenges that bedevil SMEs in Africa and suggests improved structures and collaboration as solutions to enhance SME survival.",
     buttonText: "Read More",
     icon: "/la_hands-helping.png", // Replace with an icon source or identifier as needed
-    link: "https://archive.nytimes.com/opinionator.blogs.nytimes.com/2012/06/30/the-busy-trap/",
+    link: "https://businessday.ng/opinion/article/how-structure-collaboration-can-stem-high-sme-mortality-rate/#google_vignette",
   },
   {
     title:
       "Being a Successful Entrepreneur Isn’t Only About Having the Best Ideas",
     description:
-      "Often times, having a great idea or concept is praised. However, execution is what matters in business and entrepreneurship. Ideas are the easy part; execution is the hard part.",
+      "Success depends not only on innovative ideas but also on the ability to execute them effectively. To succeed, combine good ideas with great execution.",
     buttonText: "Read More",
     icon: "/hugeicons_aircraft-game.png", // Replace with an icon source or identifier as needed
     link: "https://hbr.org/2016/08/being-a-successful-entrepreneur-isnt-only-about-having-the-best-ideas",
   },
   {
-    title: "Do Things That Don’t Scale",
+    title:
+      "Building a food delivery business in Nigeria: An entrepreneur’s journey",
     description:
-      "Paul Graham's Do Things That Don’t Scale challenges the notion that startups must focus on automation and efficiency from the start. Instead, he emphasizes importance of doing labor-intensive tasks in the early days of a startup to acquire users and establish traction",
+      "How we made it in Africa editor-in-chief speaks with FoodCourt co-founder and CEO Henry Nneji about his journey building the business.",
     buttonText: "Read More",
     icon: "/Vector.png", // Replace with an icon source or identifier as needed
-    link: "https://paulgraham.com/ds.html",
+    link: "https://www.howwemadeitinafrica.com/building-a-food-delivery-business-in-nigeria-an-entrepreneurs-journey/179379/ ",
   },
 ];
